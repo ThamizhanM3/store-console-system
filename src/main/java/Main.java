bb083b8918a5fc00a2a1a5bad34daba249bd394a
Mithelesh <105916644+ThamizhanM3@users.noreply.github.com>
2024-03-19 04:55:45 +0000

    replit code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,29 @@ public class Main {
     Cart cart = new Cart();
 
     Load l = new Load(db);
-    
+
     Product product = new Product("Product1", 10, "Category1");
     product.display();
     db.display();
 
     Menu menu = new Menu(cart, db);
     cart.displayCart();
-      
+
   }
 
-  /* M E N U....... M A I N  ILLA */
- /* poyeru
-  athu ila
-  irritate akuthu
-  no
-  bye
-  https://replit.com/@THAMIZHANM3/StoreConsole#src/main/java/Menu.java
-    // @Test
-    eii
-    
-    // void addition() {
-    //     assertEquals(2, 1 + 1);
-    // }*/
+  /* M E N U....... M A I N ILLA */
+  /*
+   * poyeru
+   * athu ila
+   * irritate akuthu
+   * no
+   * bye
+   * https://replit.com/@THAMIZHANM3/StoreConsole#src/main/java/Menu.java
+   * // @Test
+   * eii
+   * 
+   * // void addition() {
+   * // assertEquals(2, 1 + 1);
+   * // }
+   */
 }
